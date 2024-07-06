bd0f3d0758b3773f0d6eb06bcd26cf5839e8f2ca
Lavenza <msfadliii@gmail.com>
2024-07-06 17:17:18 +0700

    update

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,27 +15,6 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // User::factory(10)->create();
-
-        $userData = [
-            [
-                'email' => 'Admin@gmail.com',
-                'name'=>'admin',
-                'role' => 'admin',
-                'password' => bcrypt('admin'),
-            ],  
-
-            [
-                'email' => 'siswa1@gmail.com',
-                'name'=>'budi',
-                'role' => 'siswa',
-                'password' => bcrypt('1'),
-            ]
-        ];
-        
-       
-        foreach($userData as $key => $val){
-            User::create($val);
-        }
         $this->call([
             UserSeeder::class,
             ProvinsiSeeder::class,
